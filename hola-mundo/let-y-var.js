a6f356e89d35225ec076a27835843cf27b37fd51
Josue Martinez <josue.mart@jmcsoluciones.com>
2023-01-23 12:00:44 -0600

    Ejercicio de variables

---
+++ hola-mundo/let-y-var.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Pruebas con let y var
+//Prueba con var
+var numero = 40;
+console.log(numero);
+
+if (true){
+    var numero = 50;
+    console.log(numero);
+}
+console.log(numero);
+
+//Prueba con Let
---
+++ hola-mundo/variables.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var pais = 'España';
+var continente = 'Europa';
+var antiguedad = 2023;
+
+var datos = {
+    pais,
+    continente,
+    antiguedad,
+    concatenacion : pais + continente + antiguedad
+}
+
+
+console.log(datos);
